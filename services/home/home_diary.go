31745bc0c36520789a6976d22e1d0e19ade97003
LOFTERART <921581367@qq.com>
2020-01-12 02:47:21 +0800

    1
    1

--- services/home/home_diary.go
+++ services/home/home_diary.go
@@ -9,6 +9,7 @@ type ListDiaryService struct {
 	Page int `form:"page" json:"page" `
 	Size int `form:"size" json:"size" `
 	CommunityId int `form:"communityId" json:"communityId" `
+	ClassifyId int  `form:"classifyId" json:"classifyId"`
 }
 
 func (service *ListDiaryService) GetDiarys() serializer.Response {
@@ -26,22 +27,49 @@ func (service *ListDiaryService) GetDiarys() serializer.Response {
 
 	start := (service.Page - 1) * service.Size
 
-	if err := models.PG.Where("community_id=?",service.CommunityId).Model(models.Diary{}).Count(&total).Error; err != nil {
-		return serializer.Response{
-			Code:  50000,
-			Msg:   "数据库连接错误",
-			Error: err.Error(),
+
+	//根据前端是否传递ClassifyId 返回对应的数据
+	if service.ClassifyId>0{
+		if err := models.PG.Where("classify_id=?",service.ClassifyId).Model(models.Diary{}).Count(&total).Error; err != nil {
+			return serializer.Response{
+				Code:  50000,
+				Msg:   "数据库连接错误",
+				Error: err.Error(),
+			}
+		}
+
+		if err := models.PG.Where("classify_id=?",service.ClassifyId).Order("id desc").Limit(service.Size).Offset(start).Find(&diarys).Error; err != nil {
+			return serializer.Response{
+				Code:  50000,
+				Msg:   "数据库连接错误",
+				Error: err.Error(),
+			}
+		}
+
+	}else {
+		if err := models.PG.Where("community_id=?",service.CommunityId).Model(models.Diary{}).Count(&total).Error; err != nil {
+			return serializer.Response{
+				Code:  50000,
+				Msg:   "数据库连接错误",
+				Error: err.Error(),
+			}
+		}
+
+		if err := models.PG.Where("community_id=?",service.CommunityId).Order("id desc").Limit(service.Size).Offset(start).Find(&diarys).Error; err != nil {
+			return serializer.Response{
+				Code:  50000,
+				Msg:   "数据库连接错误",
+				Error: err.Error(),
+			}
 		}
 	}
 
-	if err := models.PG.Where("community_id=?",service.CommunityId).Order("id desc").Limit(service.Size).Offset(start).Find(&diarys).Error; err != nil {
-		return serializer.Response{
-			Code:  50000,
-			Msg:   "数据库连接错误",
-			Error: err.Error(),
-		}
-	}
+
+
 
 	return serializer.BuildListResponse(serializer.BuildDiarys(diarys), uint(total))
 
+
 }
+
+

c1700f02763c8f544b52b4f1dca607e9cc179285
LOFTERART <921581367@qq.com>
2020-01-13 20:47:01 +0800

    1
    1

--- api/postDiaryPic.go
+++ api/postDiaryPic.go
@@ -2,10 +2,10 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
-	"log"
 	"os"
 	"path/filepath"
 )
+
 //返回字段
 type Back struct {
 	Url           string `json:"url"`
@@ -17,15 +17,13 @@ func PostDiaryPic(c *gin.Context) {
 	dir, _ = filepath.Abs(filepath.Dir(os.Args[0]))
 	form, _ := c.MultipartForm()
 	files := form.File["file"]
-
 	var back Back
 	for index, file := range files {
 
 		back.Url = file.Filename
 		back.PostIndexSort = index
-		log.Println(file.Filename)
 		// 上传文件至指定目录
-		if err:=c.SaveUploadedFile(file, dir+"/static/"+file.Filename);err!=nil{
+		if err := c.SaveUploadedFile(file, dir+"/static/"+file.Filename); err != nil {
 			c.IndentedJSON(200, gin.H{
 				"code": 404,
 				"data": nil,

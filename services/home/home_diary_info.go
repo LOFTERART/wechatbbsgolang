d8eed6c5edf41e6c615332557c775781370883b0
LOFTERART <921581367@qq.com>
2020-01-13 19:48:33 +0800

    1
    1

--- services/home/home_diary_info.go
+++ services/home/home_diary_info.go
@@ -9,30 +9,23 @@ type DiaryInfoService struct {
 	Id int `form:"id" json:"id" `
 }
 
-func (service *DiaryInfoService)GetDiaryInfo(userId uint) serializer.Response {
-
+func (service *DiaryInfoService) GetDiaryInfo(userId uint) serializer.Response {
 
 	var DiaryInfo models.Diary
 
 	models.PG.
-		Where("id=?",uint(service.Id)).
-		Preload("UserInfo").
-		Preload("SubTopicInfo").
-		Preload("CommunityInfo").
+		Where("id=?", uint(service.Id)).
 		First(&DiaryInfo)
-
 
 	var users []models.User
 
-	var ids  []uint
+	var ids []uint
 
-	for _,v:=range DiaryInfo.UserLikeId{
+	for _, v := range DiaryInfo.UserLikeId {
 		ids = append(ids, uint(v))
 	}
 
 	models.PG.Where("id in (?)", ids).Find(&users)
-
-
 
 	return serializer.Response{
 		Code:  0,

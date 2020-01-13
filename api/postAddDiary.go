0c20111feb732fb3afb6f026070b1d62265d760f
LOFTERART <921581367@qq.com>
2020-01-13 15:06:02 +0800

    1
    1

--- api/postAddDiary.go
+++ api/postAddDiary.go
@@ -3,14 +3,18 @@ package api
 import (
 	"QUZHIYOU/services/home"
 	"github.com/gin-gonic/gin"
+	"strconv"
 )
 
 func PostAddDiary(c *gin.Context)  {
 
+	userid := c.Request.Header.Get("userId")
+	i, _ := strconv.Atoi(userid)
+
 	var dia home.AddDiaryService
 
 	if err:=c.ShouldBind(&dia);err==nil{
-		res:=dia.AddDiary()
+		res:=dia.AddDiary(uint(i))
 		c.JSON(200,&res)
 	}else {
 		c.JSON(200, ErrorResponse(err))
--- services/home/home_adddiary.go
+++ services/home/home_adddiary.go
@@ -7,39 +7,39 @@ import (
 )
 
 type AddDiaryService struct {
-	Avatar    string `form:"avatar" json:"avatar" `
-	Name      string `form:"name" json:"name" `
-	Content   string `form:"content" json:"content" `
-	Address   string `form:"address" json:"address" `
-	Community string `form:"community" json:"community"`
-	Photos []string `form:"photos" json:"photos"`
-	Tag string `form:"tag" json:"tag"`
+	UserId      uint
+	Content     string   `form:"content" json:"content" `
+	Address     string   `form:"address" json:"address" `
+	Community   string   `form:"community" json:"community"`
+	Photos      []string `form:"photos" json:"photos"`
+	Tag         string   `form:"tag" json:"tag"`
 	PhotosThumb []string `form:"photosthumb" json:"photosthumb"`
-	CommunityId uint `form:"communityId" json:"communityId" `  //社区id
-	ClassifyId uint `form:"classifyId" json:"classifyId" `    //标签ID
-	SubTopicId uint `form:"sub_topic_id" json:"sub_topic_id"`    //标签ID
+	CommunityId uint     `form:"communityId" json:"communityId" `  //社区id
+	ClassifyId  uint     `form:"classifyId" json:"classifyId" `    //标签ID
+	SubTopicId  uint     `form:"sub_topic_id" json:"sub_topic_id"` //标签ID
 }
 
-func (diary *AddDiaryService) AddDiary() serializer.Response {
+func (diary *AddDiaryService) AddDiary(userId uint) serializer.Response {
 
 	dia := models.Diary{
-		Content:   diary.Content,
-		Address:   diary.Address,
-		Community: diary.Community,
-		Photos:diary.Photos,
-		PhotosThumb:diary.Photos,
-		Tag:diary.Tag,
-		CommunityId:diary.CommunityId,
-		SubTopicId:diary.SubTopicId,
-		ClassifyId:diary.ClassifyId,
+		UserId:      userId,
+		Content:     diary.Content,
+		Address:     diary.Address,
+		Community:   diary.Community,
+		Photos:      diary.Photos,
+		PhotosThumb: diary.Photos,
+		Tag:         diary.Tag,
+		CommunityId: diary.CommunityId,
+		SubTopicId:  diary.SubTopicId,
+		ClassifyId:  diary.ClassifyId,
 	}
 
 	//创建话题
 	models.PG.Create(&dia)
 	//更新tag sendNum
-	 var subTopic models.SubTopic
-	subTopic.ID=diary.ClassifyId
-	models.PG.Model(&subTopic).UpdateColumn("send_num",gorm.Expr("send_num + ?", 1))
+	var subTopic models.SubTopic
+	subTopic.ID = diary.ClassifyId
+	models.PG.Model(&subTopic).UpdateColumn("send_num", gorm.Expr("send_num + ?", 1))
 
 	return serializer.Response{
 		Code:  0,

9957d93519db5aae07a36ec53996b2b36a960a33
LOFTERART <921581367@qq.com>
2020-01-14 16:52:15 +0800

    1
    1

--- api/postDiaryPic.go
+++ api/postDiaryPic.go
@@ -1,14 +1,13 @@
 package api
 
 import (
-	"fmt"
 	"github.com/disintegration/imaging"
 	"github.com/gin-gonic/gin"
 	"image"
 	"image/color"
+	"log"
 	"os"
 	"path/filepath"
-	"log"
 )
 
 //返回字段
@@ -19,14 +18,11 @@ type Back struct {
 
 func PostDiaryPic(c *gin.Context) {
 	//获取文件路径
-	dir, _ = filepath.Abs(filepath.Dir(os.Args[0]))
+	dir, _ := filepath.Abs(filepath.Dir(os.Args[0]))
 	form, _ := c.MultipartForm()
 	files := form.File["file"]
 	var back Back
 	for index, file := range files {
-
-
-
 
 		back.Url = file.Filename
 		back.PostIndexSort = index
@@ -40,17 +36,14 @@ func PostDiaryPic(c *gin.Context) {
 			return
 		}
 
-		src, err := imaging.Open(dir+"/static/"+file.Filename)
+		src, err := imaging.Open(dir + "/static/" + file.Filename)
 		if err != nil {
 			log.Fatalf("failed to open image: %v", err)
 		}
 		src = imaging.Resize(src, 300, 300, imaging.Lanczos)
-
-		fmt.Println(src,"-------src--------")
 		dst := imaging.New(300, 300, color.NRGBA{255, 255, 255, 0})
 		dst = imaging.Paste(dst, src, image.Pt(0, 0))
-		imaging.Save(dst,dir+"/static/"+"b.jpg")
-
+		imaging.Save(dst, dir+"/static/"+"b.jpg")
 
 		c.IndentedJSON(200, gin.H{
 			"code": 0,

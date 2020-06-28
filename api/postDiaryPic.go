c4d245577bf0fd352cf62a87011801266e45dbab
LOFTERART <439017006@qq.com>
2020-06-28 20:17:38 +0800

    1

--- api/postDiaryPic.go
+++ api/postDiaryPic.go
@@ -7,18 +7,24 @@ import (
 	"strings"
 )
 
+//返回字段
+type Back struct {
+	Url           string `json:"url"`
+	PostIndexSort int    `json:"post_indexSort"`
+}
 
 func PostDiaryPic(c *gin.Context) {
 	//获取文件路径
 	//dir, _ := os.Getwd()
 	form, _ := c.MultipartForm()
 	files := form.File["file"]
-
+	var back Back
 	var name string
-	for _, file := range files {
-
+	for index, file := range files {
 		ext := strings.ToLower(path.Ext(file.Filename))
 		name = utils.RandStringRunes(10) + ext
+		back.Url = name
+		back.PostIndexSort = index
 
 		// 上传文件至指定目录
 		if err := c.SaveUploadedFile(file, "./static/"+name); err != nil {
@@ -32,9 +38,7 @@ func PostDiaryPic(c *gin.Context) {
 
 		c.JSON(200, gin.H{
 			"code": 0,
-			"data": gin.H{
-				"imagName":name,
-			},
+			"data": back,
 			"msg":  "ok",
 		})
 	}
--- services/home/home_adddiary.go
+++ services/home/home_adddiary.go
@@ -11,8 +11,8 @@ type AddDiaryService struct {
 	Content     string   `form:"content" json:"content" `
 	Address     string   `form:"address" json:"address" `
 	Community   string   `form:"community" json:"community"`
-	Photos      []string `form:"photos" json:"photos"`
-	PhotosThumb []string `form:"photosthumb" json:"photosthumb"`
+	Photos      string `form:"photos" json:"photos"`
+	PhotosThumb string `form:"photosthumb" json:"photosthumb"`
 	CommunityId uint     `form:"communityId" json:"communityId" `  //社区id
 	ClassifyId  uint     `form:"classifyId" json:"classifyId" `    //标签ID
 	SubTopicId  uint     `form:"sub_topic_id" json:"sub_topic_id"` //标签ID
@@ -24,8 +24,8 @@ func (diary *AddDiaryService) AddDiary(userId uint) serializer.Response {
 		UserId:      userId,
 		Content:     diary.Content,
 		Address:     diary.Address,
-		//Photos:      diary.Photos,
-		//PhotosThumb: diary.Photos,
+		Photos:      diary.Photos,
+		PhotosThumb: diary.Photos,
 		CommunityId: diary.CommunityId,
 		SubTopicId:  diary.SubTopicId,
 		ClassifyId:  diary.ClassifyId,

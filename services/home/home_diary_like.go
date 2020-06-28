afddfbb030a5ce340c50e549a8d22092d3c4e13f
LOFTERART <439017006@qq.com>
2020-06-29 03:01:24 +0800

    1

--- services/home/home_diary_like.go
+++ services/home/home_diary_like.go
@@ -3,7 +3,9 @@ package home
 import (
 	"QUZHIYOU/models"
 	"QUZHIYOU/serializer"
+	"fmt"
 	"github.com/jinzhu/gorm"
+	"strings"
 )
 
 type DiaryLikeService struct {
@@ -20,26 +22,28 @@ func (service *DiaryLikeService) LikeDiary(userid []string) serializer.Response 
 	}
 
 	models.DB.First(&diary)
+	UserLikeArrayId := strings.Split(diary.UserLikeId, ",")
 
 	if service.Type {
-		//for k, v := range diary.UserLikeId {
-		//	if userid[0] == v {
-		//
-		//		diary.UserLikeId = append(diary.UserLikeId[:k], diary.UserLikeId[k+1:]...)
-		//
-		//	}
-		//}
+		for k, v := range UserLikeArrayId {
+			if userid[0] == v {
+				//diary.UserLikeId = append(diary.UserLikeId[:k], diary.UserLikeId[k+1:]...)
+				UserLikeArrayId = append(UserLikeArrayId[:k], UserLikeArrayId[k+1:]...)
+			}
+		}
+
+		fmt.Println(UserLikeArrayId,"------UserLikeArrayId-------")
 
 		models.DB.Model(&diary).
 			Updates(map[string]interface{}{"like": diary.Like - 1, "user_like_id": diary.UserLikeId})
 
 	} else {
-		//for _, v := range diary.UserLikeId {
-		//	if userid[0] != v {
-		//		userid = append(userid, v)
-		//	}
-		//
-		//}
+		for _, v := range UserLikeArrayId {
+			if userid[0] != v {
+				userid = append(userid, v)
+			}
+
+		}
 		models.DB.Model(&diary).Updates(map[string]interface{}{"like": diary.Like + 1})
 		models.DB.Exec("UPDATE diary SET user_like_id = user_like_id||?  WHERE id= ? ", userid, service.DiaryId)
 
